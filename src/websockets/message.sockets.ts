6f4f5e7c95439d0a6deb58b151893f544bc9218b
Santiago Feijoo <santiago.feijoo96@gmail.com>
2025-12-07 23:48:03 -0300

    cuando un cliente se conecta a un room, se va del chat general y entra al caht del room. Cuando sale, se va de ese chat y entra al general

--- src/websockets/message.sockets.ts
+++ src/websockets/message.sockets.ts
@@ -2,13 +2,17 @@ import { Socket } from "socket.io"
 import { MessageService } from "../services"
 import { MessageEvents, CreateMessageDto } from "../shared"
 import { Server } from "socket.io";
+import { GENERAL_CHAT_CHANNEL } from "../shared/constants";
 
 export const registerMessageEvents = (socket: Socket, io: Server) => {
 
+  socket.join(GENERAL_CHAT_CHANNEL)
+  
   socket.on(MessageEvents.CREATE, (msgDto : CreateMessageDto) => {
+    console.log("new message to ", msgDto)
     const newMessage = MessageService.addMessage(msgDto)
     if(!msgDto.roomId){
-      io.emit(MessageEvents.CREATED, newMessage) // lo reciben todos
+      io.to(GENERAL_CHAT_CHANNEL).emit(MessageEvents.CREATED, newMessage)
     }else{
       io.to(msgDto.roomId).emit(MessageEvents.CREATED, newMessage) // lo reciben solo los sockets que estan escuchando en el canal del roomId
     }
--- src/websockets/room.sockets.ts
+++ src/websockets/room.sockets.ts
@@ -2,6 +2,7 @@ import { Socket } from "socket.io"
 import { RoomService } from "../services"
 import { RoomEvents, Room, CreateRoomDto, JoinRoomDto, Player } from "../shared"
 import { Server } from "socket.io";
+import { GENERAL_CHAT_CHANNEL } from "../shared/constants";
 
 export const emitRoomList = (socket: { emit: (arg0: RoomEvents, arg1: Room[]) => void }) => {
   socket.emit(RoomEvents.LIST, RoomService.getRooms())
@@ -21,6 +22,8 @@ export const registerAllRoomEvents = (socket: Socket, io: Server) => {
     // Si el usuario ya estaba en la sala, lo ignoro
     if(RoomService.isPlayerInRoom(incomingPlayer)) return
     
+    // Cuando un jugador se une a un room, se une al canal especifico y se va del general
+    socket.leave(GENERAL_CHAT_CHANNEL)
     console.log(`New player ${incomingPlayer.username} trying to join ${incomingPlayer.roomId}`)
     const updatedRoom = RoomService.addPlayerToRoom(incomingPlayer)
     socket.join(incomingPlayer.roomId)
@@ -31,7 +34,9 @@ export const registerAllRoomEvents = (socket: Socket, io: Server) => {
   socket.on(RoomEvents.LEAVE, (outcomingPlayer: JoinRoomDto) => {
     if(!RoomService.isPlayerInRoom(outcomingPlayer)) return
     
+    // Cuando un jugador se va de un room, se sale del canal del room y entra al chat general
     console.log(`New player ${outcomingPlayer.username} is trying to LEAVE from ${outcomingPlayer.roomId}`)
+    socket.join(GENERAL_CHAT_CHANNEL)
     const updatedRoom = RoomService.removePlayerfromRoom(outcomingPlayer)
     socket.leave(outcomingPlayer.roomId)
     io.emit(RoomEvents.USER_LEFT, updatedRoom)
