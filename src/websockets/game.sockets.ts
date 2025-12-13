53f242c28a56ff0ac4991ed5a8564896f94772f4
Santiago Feijoo <santiago.feijoo96@gmail.com>
2025-12-13 01:47:41 -0300

    submit word dto

--- src/websockets/game.sockets.ts
+++ src/websockets/game.sockets.ts
@@ -1,6 +1,12 @@
 import { Server, Socket } from "socket.io";
-import { Game, GameEvents } from "../shared";
+import { Game, GameEvents, SubmitWordDto } from "../shared";
 
 export const registerGameEvents = (socket: Socket, io: Server, game: Game) => {
-    // socket.on()
+    socket.on(GameEvents.SUBMIT_WORD, (submitWordDto: SubmitWordDto) => {
+        // Cuando alguien hace una jugada, registro el Move en el array de Moves con el current round
+        // Calculo el siguiente turno
+        // Emito Evento word Submitted.
+        // Si ya todos jugaron, emito otro evento para activar la siguiente fase
+        console.log("word submitted ", submitWordDto)
+    })
 }
