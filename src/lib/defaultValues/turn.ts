79f83eec7f60ebe1852e84cda8b6d8036a8be8ba
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-01-13 13:13:19 -0300

    tipado en default turn

--- src/lib/defaultValues/turn.ts
+++ src/lib/defaultValues/turn.ts
@@ -1,5 +1,8 @@
-export const defaultTurn = {
+import { Turn } from "../types";
+
+export const defaultTurn: Turn = {
     player: "",
     duration: 0,
-    startedAt: 0
+    startedAt: 0,
+    endsAt: 0
 }
